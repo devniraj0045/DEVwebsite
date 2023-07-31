cac5cc87369202d9c08103c406e0f94423a66cfb
devniraj0045 <devniraj2003@gmail.com>
2023-07-31 20:46:35 +0545

    link_home

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -46,7 +46,7 @@ const Navbar = () => {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav ms-auto mb-2 mb-lg-0 text-center">
                     <li className="nav-item">
-                    <Link className="nav-link  " aria-current="page" to="/">HOME</Link>
+                    <Link className="nav-link  " aria-current="page" to="/DEVwebsite">HOME</Link>
                     </li>
                     <li className="nav-item">
                     <Link className="nav-link" to ="/MySkills">MY SKILLS</Link>
